1eff8863d5510933a0603f190d4a260d8a64128a
Amit Kumar <Kumaramit640690@gmail.com>
2024-04-04 13:29:25 +0530

    abc

--- notemanager with scheduler/Mini assignment/backend/mini-java-assignment/src/main/java/com/nagarro/mini/java/assignment/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nagarro.mini.java.assignment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nagarro.mini.java.assignment.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
